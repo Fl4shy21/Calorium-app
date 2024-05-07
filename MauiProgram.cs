f881d2d631ce4c91a34effad47fb4cca88109fb1
Fl4shy21 <132828495+Fl4shy21@users.noreply.github.com>
2024-05-07 21:09:52 +0300

    Add files via upload

---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+using Camera.MAUI;
+using ZXing.Net.Maui.Controls;
+
+
+namespace Calorium
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCameraView()
+                .UseBarcodeReader()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Altone Trial-Regular.ttf", "AltoneRegular");
+                    fonts.AddFont("Altone Trial-Bold.ttf", "AltoneBold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
