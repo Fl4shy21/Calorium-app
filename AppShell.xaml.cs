e83f49d92ac5487693a3119155281bf2fff26a7e
Fl4shy21 <132828495+Fl4shy21@users.noreply.github.com>
2024-05-17 21:44:53 +0300

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Calorium.Views;
+
+namespace Calorium
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MenuPage), typeof(MenuPage));
+            Routing.RegisterRoute(nameof(MaintenanceLevelCalculator), typeof(MaintenanceLevelCalculator));
+            Routing.RegisterRoute(nameof(GetToKnow), typeof(GetToKnow));
+            Routing.RegisterRoute(nameof(ChooseYourPlan), typeof(ChooseYourPlan));
+            Routing.RegisterRoute(nameof(ContactCreator), typeof(ContactCreator));
+            Routing.RegisterRoute(nameof(Push_Pull_Legs), typeof(Push_Pull_Legs));
+            Routing.RegisterRoute(nameof(ArnoldSplit), typeof(ArnoldSplit));
+            Routing.RegisterRoute(nameof(HeavyDuty), typeof(HeavyDuty));
+            Routing.RegisterRoute(nameof(BMRImperial), typeof(BMRImperial));
+            Routing.RegisterRoute(nameof(BMRMetric), typeof(BMRMetric));
+            Routing.RegisterRoute(nameof(QRScanner), typeof(QRScanner));
+            Routing.RegisterRoute(nameof(ArnoldSplit_Beginner), typeof(ArnoldSplit_Beginner));
+            Routing.RegisterRoute(nameof(ArnoldSplit_Advanced), typeof(ArnoldSplit_Advanced));
+        }
+    }
+}
