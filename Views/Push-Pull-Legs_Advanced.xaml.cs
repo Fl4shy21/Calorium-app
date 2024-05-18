4dfb5233cc5c59bc3336887a8829bbb3785bdd34
Fl4shy21 <132828495+Fl4shy21@users.noreply.github.com>
2024-05-18 17:30:00 +0300

    Add files via upload

---
+++ Views/Push-Pull-Legs_Advanced.xaml.cs
@@ -0,0 +1,9 @@
+namespace Calorium.Views;
+
+public partial class Push_Pull_Legs_Advanced : ContentPage
+{
+	public Push_Pull_Legs_Advanced()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/Push-Pull-Legs_Beginner.xaml.cs
@@ -0,0 +1,9 @@
+namespace Calorium.Views;
+
+public partial class Push_Pull_Legs_Beginner : ContentPage
+{
+	public Push_Pull_Legs_Beginner()
+	{
+		InitializeComponent();
+	}
+}
