982227d53812f88f353ef62d2236612e85887192
Fl4shy21 <132828495+Fl4shy21@users.noreply.github.com>
2024-06-20 11:29:47 +0300

    Add files via upload

---
+++ Views/QRScanner.xaml.cs
@@ -0,0 +1,53 @@
+using Microsoft.Maui.Controls;
+using System;
+using System.Linq;
+using ZXing.Net.Maui;
+
+namespace Calorium.Views
+{
+    public partial class QRScanner : ContentPage
+    {
+        private string scannedURL;
+
+        public QRScanner()
+        {
+            InitializeComponent();
+            barcodeReader.Options = new BarcodeReaderOptions()
+            {
+                Formats = BarcodeFormat.QrCode,
+                AutoRotate = true,
+                Multiple = true
+            };
+        }
+
+        private void CameraBarcodeReaderView_BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+        {
+            var first = e.Results.FirstOrDefault();
+
+            if (first is null)
+                return;
+
+            scannedURL = first.Value;
+
+            Dispatcher.DispatchAsync(async () =>
+            {
+                barcodeResult.Text = scannedURL;
+                linkBtn.IsVisible = true;
+                await DisplayAlert("Barcode Detected", scannedURL, "OK");
+            });
+        }
+
+        private async void BackToMenuBtn_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//MenuPage");
+        }
+
+        private async void linkBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(scannedURL))
+            {
+                await Launcher.OpenAsync(new Uri(scannedURL));
+            }
+        }
+    }
+}
