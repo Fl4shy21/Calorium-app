6895daa103c677a1e9f88aaf666e659a5ccc5cdf
Fl4shy21 <132828495+Fl4shy21@users.noreply.github.com>
2024-07-05 09:16:53 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Calorium.Views;
+
+namespace Calorium
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new OnboardingPage();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Calorium.Views;
+
+namespace Calorium
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MenuPage), typeof(MenuPage));
+            Routing.RegisterRoute(nameof(MaintenanceLevelCalculator), typeof(MaintenanceLevelCalculator));
+            Routing.RegisterRoute(nameof(GetToKnow), typeof(GetToKnow));
+            Routing.RegisterRoute(nameof(ChooseYourPlan), typeof(ChooseYourPlan));
+            Routing.RegisterRoute(nameof(ChooseYourPlan_Beginner), typeof(ChooseYourPlan_Beginner));
+            Routing.RegisterRoute(nameof(ChooseYourPlan_Advanced), typeof(ChooseYourPlan_Advanced));
+            Routing.RegisterRoute(nameof(ContactCreator), typeof(ContactCreator));
+            Routing.RegisterRoute(nameof(HeavyDuty), typeof(HeavyDuty));
+            Routing.RegisterRoute(nameof(QRScanner), typeof(QRScanner));
+            Routing.RegisterRoute(nameof(ArnoldSplit_Beginner), typeof(ArnoldSplit_Beginner));
+            Routing.RegisterRoute(nameof(ArnoldSplit_Advanced), typeof(ArnoldSplit_Advanced));
+            Routing.RegisterRoute(nameof(Push_Pull_Legs_Beginner), typeof(Push_Pull_Legs_Beginner));
+            Routing.RegisterRoute(nameof(Push_Pull_Legs_Advanced), typeof(Push_Pull_Legs_Advanced));
+            Routing.RegisterRoute(nameof(BroSplit), typeof(BroSplit));
+            Routing.RegisterRoute(nameof(Diet), typeof(Diet));
+            Routing.RegisterRoute(nameof(Training), typeof(Training));
+            Routing.RegisterRoute(nameof(Supplements), typeof(Supplements));
+            Routing.RegisterRoute(nameof(OnboardingPage), typeof(OnboardingPage));
+        }
+    }
+}
